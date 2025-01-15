37071beae4344d6105b9b9051469e44c036a35df
Honchar Denys <crackeraki@gmail.com>
2025-01-15 23:03:10 +0200

    added meta to env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,11 @@ export const environment = {
 	roles: [],
 	production: true,
 	appId: 'test',
-	url: ''
+	url: '',
+	meta: {
+		title: 'Web Art Work',
+		description:
+			'An amazing solution to build web or mobile app for your business',
+		icon: 'https://webart.work/api/user/cdn/waw-logo.png'
+	}
 };
