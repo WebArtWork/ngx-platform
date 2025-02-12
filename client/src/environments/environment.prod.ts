aea00e810f602a2a3c10cafa7e3d85be50e4225f
Honchar Denys <crackeraki@gmail.com>
2025-02-13 01:21:33 +0200

    make again icon work

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -8,6 +8,6 @@ export const environment = {
 		description:
 			'An amazing solution to build web or mobile app for your business',
 		icon: 'https://webart.work/api/user/cdn/waw-logo.png',
-		logo: 'assets/logo.png'
+		logo: ''
 	}
 };
