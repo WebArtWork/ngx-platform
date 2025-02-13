8a2efc7256711bbe4efa82f8d27ef59e48d0d8c0
Honchar Denys <crackeraki@gmail.com>
2025-02-13 10:30:25 +0200

    fixed favicon

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
 		title: 'Web Art Work',
 		description:
 			'An amazing solution to build web or mobile app for your business',
-		icon: 'https://webart.work/api/user/cdn/waw-logo.png',
+		icon: 'https://ngx.webart.work/assets/logo.png',
 		logo: ''
 	}
 };
