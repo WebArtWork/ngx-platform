ea22b212df59ee85459a420b5dbea80236ad2804
Honchar Denys <crackeraki@gmail.com>
2025-09-03 22:27:53 +0300

    upgrade angular

--- server/bird/bird.collection.js
+++ server/bird/bird.collection.js
@@ -4,24 +4,9 @@ module.exports = async function (waw) {
 		description: String,
 		url: { type: String, sparse: true, trim: true, unique: true },
 		data: {},
-		author: {
-			type: waw.mongoose.Schema.Types.ObjectId,
-			ref: "User",
-		},
-		moderators: [
-			{
-				type: waw.mongoose.Schema.Types.ObjectId,
-				sparse: true,
-				ref: "User",
-			},
-		],
 	});
 
-	Schema.methods.create = function (obj, user, waw) {
-		this.author = user._id;
-
-		this.moderators = [user._id];
-
+	Schema.methods.create = function (obj) {
 		this.name = obj.name;
 
 		this.description = obj.description;
