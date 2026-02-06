afd893713c68bc3e3d3bca2a4ec8155a3e907d35
Honchar Denys <crackeraki@gmail.com>
2026-02-06 07:59:26 +0200

    lintify

--- client/src/app/page-components/marked/marked.component.spec.ts
+++ client/src/app/page-components/marked/marked.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MarkedComponent } from './marked.component';
 
 describe('MarkedComponent', () => {
-  let component: MarkedComponent;
-  let fixture: ComponentFixture<MarkedComponent>;
+	let component: MarkedComponent;
+	let fixture: ComponentFixture<MarkedComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MarkedComponent]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [MarkedComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(MarkedComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+		fixture = TestBed.createComponent(MarkedComponent);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
