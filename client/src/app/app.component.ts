e2234f8f17a7366f02d4b83137c745004959936b
Honchar Denys <crackeraki@gmail.com>
2026-03-04 23:22:19 +0200

    cleanup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from '@module/user';
 import { ThemeService } from 'wacom';
 
 @Component({
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [RouterOutlet],
 	selector: 'app-root',
-	templateUrl: './app.component.html',
+	template: '<router-outlet />',
 })
 export class AppComponent {
-	private readonly _httpService = inject(UserService);
-
 	constructor() {
 		inject(ThemeService).init();
 	}
